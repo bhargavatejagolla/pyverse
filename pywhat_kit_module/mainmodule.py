6e81a43dc52fbaaa8eba422f7c210d5387e5e139
bhargavatejagolla <bhargavatejagolla024@gmail.com>
2025-08-14 22:27:24 +0530

    Add files via upload

---
+++ pywhat_kit_module/mainmodule.py
@@ -0,0 +1,5 @@
+import pywhatkit as pk 
+txt = """hello world my name is bhargava teja what the hell is going on and the
+people are so much angry adn the what making making them so angry"""
+pk.text_to_handwriting(txt,"demo.png",[0,0,138])
+print("finsihed home work")
