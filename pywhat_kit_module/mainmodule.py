1500ba815f96d7d882f808f2a592d4e85e595365
bhargavatejagolla <bhargavatejagolla024@gmail.com>
2025-08-14 22:34:26 +0530

    Update mainmodule.py

--- pywhat_kit_module/mainmodule.py
+++ pywhat_kit_module/mainmodule.py
@@ -1,5 +1,5 @@
 import pywhatkit as pk 
-txt = """hello world my name is bhargava teja what the hell is going on and the
-people are so much angry adn the what making making them so angry"""
+txt = """Text-based coding is the process of writing instructions in a programming language using text characters. This involves using a specific syntax (rules and structure) of the chosen language to communicate with a computer. It's the method used by professional programmers 
+and follows a different approach than block-based coding, which relies on visual"""
 pk.text_to_handwriting(txt,"demo.png",[0,0,138])
 print("finsihed home work")
